998e6498ec40e51e503f122b869aeb980df3d416
Eric <ejefferson50@gmail.com>
2019-08-15 12:35:49 -0400

    First Commit

---
+++ src/main/java/com/server/project/comixconnexion/ComixConnexionApplication.java
@@ -0,0 +1,13 @@
+package com.server.project.comixconnexion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComixConnexionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComixConnexionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/project/comixconnexion/ComixConnexionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.server.project.comixconnexion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComixConnexionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
